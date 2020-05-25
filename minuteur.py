a912122612510c4679b888567f46e68faef880a0
GamerKit <65911022+GamerKit@users.noreply.github.com>
2020-05-25 17:30:43 +0200

    minuteur

---
+++ minuteur.py
@@ -0,0 +1,31 @@
+import time
+
+seconde=0
+minute=0
+heure=0
+
+continuer = True
+continuer2= True
+
+
+while continuer == True:
+    time.sleep(1)
+    seconde=seconde+1
+    print(heure, "h", minute, "min", seconde, "s")
+
+
+    if seconde == 60:
+        continuer = False
+        seconde = 0
+        continuer = True
+        minute=minute+1
+
+    if minute == 60:
+        continuer = False
+        seconde = 0
+        minute = 0
+        continuer = True
+        heure=heure+1
+
+
+
